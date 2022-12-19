30ec15c8a3bd9a9c90d8885d6f0c7a5d4691fa64
F <juliaferrer98@gmail.com>
2022-12-19 19:53:57 +0100

    comptador

--- src/exercicis/2-comptador/Comptador.js
+++ src/exercicis/2-comptador/Comptador.js
@@ -1,1 +1,18 @@
 import "./Comptador.css";
+import { useState } from "react";
+
+function Comptador() {
+     
+    const [counterNumber, setSuma1] = useState(0) 
+        
+    return (
+      <div className="display">
+        <p className="numberBox">{counterNumber}</p>
+        <button onClick={() => setSuma1( (counterNumber>9) ? counterNumber : (valorActual) => valorActual+1)}>+1</button>
+        <button onClick={() => setSuma1( (counterNumber<1) ? counterNumber : (valorActual) => valorActual-1)}>-1</button>
+        <button onClick={() => setSuma1( (counterNumber>5) ? counterNumber : (valorActual) => valorActual+5)}>+5</button>
+      </div>
+    );
+  }
+  
+  export default Comptador;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import "./Home.css";
 import Graella from "../exercicis/1-graellaFotos/Graella";
+import Comptador from "../exercicis/2-comptador/Comptador";
 
 function Home() {
-  return <Graella />;
+  return( 
+  <>
+    <Graella />;
+    <Comptador/>;
+  </>
+  )
 }
 
 export default Home;
