ec03c330de7b557d82c188fcfd864e82f18667c3
F <juliaferrer98@gmail.com>
2022-12-19 20:09:47 +0100

    comptador amb CSS decent

--- src/exercicis/2-comptador/Comptador.js
+++ src/exercicis/2-comptador/Comptador.js
@@ -8,9 +8,11 @@ function Comptador() {
     return (
       <div className="display">
         <p className="numberBox">{counterNumber}</p>
-        <button onClick={() => setSuma1( (counterNumber>9) ? counterNumber : (valorActual) => valorActual+1)}>+1</button>
-        <button onClick={() => setSuma1( (counterNumber<1) ? counterNumber : (valorActual) => valorActual-1)}>-1</button>
-        <button onClick={() => setSuma1( (counterNumber>5) ? counterNumber : (valorActual) => valorActual+5)}>+5</button>
+        <div>
+            <button className="numberButton" onClick={() => setSuma1( (counterNumber>9) ? counterNumber : (valorActual) => valorActual+1)}>+1</button>
+            <button className="numberButton" onClick={() => setSuma1( (counterNumber<1) ? counterNumber : (valorActual) => valorActual-1)}>-1</button>
+        </div>
+             <button className="numberButton2" onClick={() => setSuma1( (counterNumber>5) ? counterNumber : (valorActual) => valorActual+5)}>+5</button>
       </div>
     );
   }
