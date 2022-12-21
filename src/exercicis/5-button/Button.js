8a3d51f02fbb4db846c4caccd57e21b32970fb35
F <juliaferrer98@gmail.com>
2022-12-21 18:51:39 +0100

    premut afegit a button

--- src/exercicis/5-button/Button.js
+++ src/exercicis/5-button/Button.js
@@ -4,14 +4,24 @@ import { useState } from "react";
 function Button () {
     const [color, setColor] = useState (0);
     const setClassName = () => {setColor (color < 2 ? (previousColor) => previousColor + 1 : (previousColor) => previousColor = 1)}
+    
+    const [premut, setPremut] = useState (false)
+    const setPremutState = () => {setPremut ((previousState) => !previousState)}
+
     return(
         <div className="buttonContainer">
             <button 
             className={color === 0 ? "greenButton" : color === 1? "blueButton" : "uncoloredButton"} 
             onClick={setClassName}
+            onMouseDown={setPremutState}
+            onMouseUp={setPremutState}
             >Sóc un botó</button>
+            <div className="premutText">
+            {premut ? <p>Premut</p> : null}
+            </div>
         </div>
     )
 }
+
 
 export default Button;
